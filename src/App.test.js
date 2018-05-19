4850c611791b278e0bbc9a8a0d69b15ba5d29a2e
JimTheMan <mrdotjim@gmail.com>
2018-05-18 23:26:24 -0400

    added pre-commit hook

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { expect } from 'chai';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('fails hard', () => {
+  expect(2).to.equal(2)
+
+})
